2a42f279c3f34ebf460f467f8502d0f571aa0f4a
Jenn G <shimmer10@aol.com>
2019-05-04 11:30:24 -0400

    updates to js and html to accept input values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,18 @@
-$(function() {
-  console.log("Hello World");
-});
+$(function () {
+
+  $("#submit").on("click", function (e) {
+    e.preventDefault();
+    var inputName = $("#input-employee-name").val();
+    var inputRole = $("#input-role").val();
+    var inputStartDate = $("#input-start-date").val();
+    var inputMonthlyRate = $("#input-monthly-rate").val();
+
+    $("#employee-name").html(inputName);
+    $("#role").html(inputRole); 
+    $("#start-date").html(inputStartDate);
+    $("#months-worked").html("something");
+    $("#monthly-rate").html("$" + inputMonthlyRate);
+    $("#total-billed").html("something else");
+
+  });
+})
