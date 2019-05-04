60ba7bfc3242b8b1c04fe5d2ef37b123e7809f30
JONATHAN MEYER <JONATHAN.MEYER@LIBERTYMUTUAL.COM>
2019-05-04 11:47:31 -0400

    adding form data to the table reseting the form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,23 @@
-$(function () {
+$(function() {
+  $("#submit").on("click", function(e) {
+    e.preventDefault()
 
-  $("#submit").on("click", function (e) {
-    e.preventDefault();
-    var inputName = $("#input-employee-name").val();
-    var inputRole = $("#input-role").val();
-    var inputStartDate = $("#input-start-date").val();
-    var inputMonthlyRate = $("#input-monthly-rate").val();
+    var inputName = $("#input-employee-name").val()
+    var inputRole = $("#input-role").val()
+    var inputStartDate = $("#input-start-date").val()
+    var inputMonthlyRate = $("#input-monthly-rate").val()
 
-    $("#employee-name").html(inputName);
-    $("#role").html(inputRole); 
-    $("#start-date").html(inputStartDate);
-    $("#months-worked").html("something");
-    $("#monthly-rate").html("$" + inputMonthlyRate);
-    $("#total-billed").html("something else");
+    $("#emp-table").append(
+      $("<tr>").append(
+        $("<td>").text(inputName),
+        $("<td>").text(inputRole),
+        $("<td>").text(inputStartDate),
+        $("<td>").text("something"),
+        $("<td>").text("$" + inputMonthlyRate),
+        $("<td>").text("something else")
+      )
+    )
 
-  });
+    $("form").trigger("reset")
+  })
 })
