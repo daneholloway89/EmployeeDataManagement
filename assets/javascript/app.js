f40d79d7151d62b3032535ac9564b38366d8f704
JONATHAN MEYER <JONATHAN.MEYER@LIBERTYMUTUAL.COM>
2019-05-04 12:59:04 -0400

    fixed push bug added moment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,42 @@
+// DOM Ready Function
 $(function() {
-  $("#submit").on("click", function(e) {
-    e.preventDefault()
+  // firebase config
+  var firebaseConfig = {
+    apiKey: "AIzaSyCwMKdKheiwZYTsVhFO2qJsIxJF77Z6buo",
+    authDomain: "employeedatamanagement-6d663.firebaseapp.com",
+    databaseURL: "https://employeedatamanagement-6d663.firebaseio.com",
+    projectId: "employeedatamanagement-6d663",
+    storageBucket: "employeedatamanagement-6d663.appspot.com",
+    messagingSenderId: "960834757189",
+    appId: "1:960834757189:web:f8aa0cbf1039675f",
+  };
 
-    var inputName = $("#input-employee-name").val()
-    var inputRole = $("#input-role").val()
-    var inputStartDate = $("#input-start-date").val()
-    var inputMonthlyRate = $("#input-monthly-rate").val()
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  // init vars
+  var inputName = "";
+  var inputRole = "";
+  var inputStartDate = "";
+  var inputMonthlyRate = "";
+
+  // add submitt onClick handler
+  $("#submit").on("click", function(e) {
+    e.preventDefault();
+
+    inputName = $("#input-employee-name")
+      .val()
+      .trim();
+    inputRole = $("#input-role")
+      .val()
+      .trim();
+    inputStartDate = $("#input-start-date")
+      .val()
+      .trim();
+    inputMonthlyRate = $("#input-monthly-rate")
+      .val()
+      .trim();
 
     $("#emp-table").append(
       $("<tr>").append(
@@ -16,8 +47,20 @@ $(function() {
         $("<td>").text("$" + inputMonthlyRate),
         $("<td>").text("something else")
       )
-    )
+    );
 
-    $("form").trigger("reset")
-  })
-})
+    database.ref().push(
+      {
+        name: inputName,
+        role: inputRole,
+        startDate: inputStartDate,
+        monthlyRate: inputMonthlyRate,
+      },
+      function(err) {
+        console.log({ err });
+      }
+    );
+
+    $("form").trigger("reset");
+  });
+});
